8a85f86b3345fdfd131e58786f46e132447de5db
Alitor004 <irmaosgomes1302@gmail.com>
2023-05-10 20:46:30 -0300

    Correção dos erros

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,7 +2,47 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calcular.setOnClickListener{ calculateTip() }
+    }
+    fun calculateTip() {
+        val stringInTextField = binding.preco.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.vinte -> 0.20
+            R.id.dezoito -> 0.18
+            else -> 0.15
+
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.gorjetaAtivo.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.resultadoDaGorjeta.text = getString(R.string.resultadoDaGorjeta, formattedTip)
+    }
+
+}
+
+
+//Meu Comando que dua errado
+
+/*package com.example.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +52,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        binding.calcularButton.setOnClickListener{ calcularGorjeta() }
+        binding.calcular.setOnClickListener{ calcularGorjeta() }
     }
     fun calcularGorjeta(){
+        val stringTextField = binding.preco.text.toString()
+        val prec = stringTextField.toDouble()
+        val selectedId = binding.gorjetaOpicao.checkedRadioButtonId
+        val gorjetaPorcentagem = when (selectedId) {
+            R.id.vinte -> 0.20
+            R.id.dezoito -> 0.18
+            else ->0.15
+        }
+        var gorjeta = gorjetaPorcentagem * prec
+        val roundUp = binding.switch1.isChecked
+        if(roundUp){
+            gorjeta = kotlin.math.ceil(gorjeta)
+        }
+        val formatoGorjeta = NumberFormat.getCurrencyInstance().format(gorjeta)
+        //binding.resultadoDaGorjeta.text = getString(R.string.resultadoDaGorjeta, formatoGorjeta)
+        //binding.resultadoDaGorjeta.text = gorjeta.toString()
+        //binding.resultadoDaGorjeta.text = binding.preco.text.toString()
 
     }
-}
+}*/
